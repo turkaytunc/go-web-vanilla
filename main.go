c3ad182414b96f781be46bd3af1f1202bd6981d1
Türkay Tunç <turkaytunc00@gmail.com>
2021-06-26 22:00:17 +0300

    add: handler function

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func setupHandler(w http.ResponseWriter, r *http.Request) {
+
+	message := []byte("Hello World")
+	_, err := w.Write(message)
+
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", setupHandler)
+	http.ListenAndServe("localhost:4000", nil)
 }
